a556bb852bc34e1ec74476d87fad0488e5300380
Sivadas, Sibidas <Sibidas_Sivadas@intuit.com>
2017-06-29 15:31:43 +0530

    first commit

---
+++ src/HelloSpinnaker.java
@@ -0,0 +1,10 @@
+
+public class HelloSpinnaker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Spinnaker.. This is a sample app..");
+
+	}
+
+}
